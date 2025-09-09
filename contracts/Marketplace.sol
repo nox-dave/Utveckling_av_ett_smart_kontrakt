f55994ffe1bf6976a8f5207ee119e94ce20b15ce
Rasta Dave <david.borg.kevin321@gmail.com>
2025-09-09 18:18:45 +0200

    Implement basic access controls

--- contracts/Marketplace.sol
+++ contracts/Marketplace.sol
@@ -2,14 +2,41 @@
 pragma solidity ^0.8.30;
 
 contract Marketplace {
+    error NotOwner();
+    error NotAdmin();
+
     address public owner;
+    mapping(address => bool) public admins;
 
     constructor() {
         owner = msg.sender;
+        admins[msg.sender] = true;
     }
 
+    event GrantAdmin(address indexed account);
+    event RevokeAdmin(address indexed account);
+
+    fallback() external payable {}
+
+    receive() external payable {}
+
     modifier onlyOwner() {
-        require(msg.sender == owner, "not owner");
+        if (msg.sender != owner) revert NotOwner();
         _;
+    }
+
+    modifier onlyAdmin() {
+        if (!admins[msg.sender]) revert NotAdmin();
+        _;
+    }
+
+    function grantAdmin(address account) external onlyOwner {
+        admins[account] = true;
+        emit GrantAdmin(account);
+    }
+
+    function revokeAdmin(address account) external onlyOwner {
+        admins[account] = false;
+        emit RevokeAdmin(account);
     }
 }
